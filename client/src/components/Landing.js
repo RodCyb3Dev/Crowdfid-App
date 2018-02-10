d2a15a4cd06e1350b49b56e25b4e23636117c02c
rodcode47 <rodney.hammad@gmail.com>
2018-02-10 17:54:37 +0200

    make it better

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Crowdback!
+      </h1>
+      Collect feedback form your users
+    </div>
+  );
+};
+
+export default Landing;
